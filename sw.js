e78cc5ad6327a713a6cbc14e6377770a74fb254d
florie-decitre <florie.decitre@gaea21.org>
2024-07-23 15:57:30 +0200

    travaille sur l'instalation

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
     event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/manifest.json',
-          '/icon-192x192.png',
-          '/icon-512x512.png'
-        ]);
-      })
+        caches.open("v1").then(function (cache) {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/styles.css",
+                "/manifest.json",
+                "/icon-192x192.png",
+                "/icon-512x512.png",
+            ]);
+        })
     );
-  });
-  
-  self.addEventListener('fetch', function(event) {
+});
+
+self.addEventListener("fetch", function (event) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
     );
-  });
-  
+});
